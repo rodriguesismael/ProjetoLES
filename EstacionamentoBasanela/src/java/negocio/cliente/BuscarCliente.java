33e35e54cb12d5aafc46e1ca70339d08bec2a436
rodriguesismael <isrodrigues5@gmail.com>
2014-05-09 00:17:23 -0300

    busca de cliente: parcial

--- EstacionamentoBasanela/src/java/negocio/cliente/BuscarCliente.java
+++ EstacionamentoBasanela/src/java/negocio/cliente/BuscarCliente.java
@@ -6,6 +6,7 @@
 
 package negocio.cliente;
 
+import com.sun.security.ntlm.Client;
 import controller.UpdateInterface;
 
 import modelo.cliente.Cliente;
@@ -17,7 +18,7 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
+import negocio.outro.EfetuarLogin;
 
 /**
  *
@@ -26,6 +27,34 @@ import javax.servlet.http.HttpServletResponse;
 public class BuscarCliente implements UpdateInterface{
     
     public void executa(HttpServletRequest request, HttpServletResponse response){
-        
+       PrintWriter out = null;
+       try{
+           out = response.getWriter();
+       }catch(IOException exc){
+           Logger.getLogger(EfetuarLogin.class.getName()).log(Level.SEVERE,null,exc);
+       }
+       
+       Cliente cliente = new Cliente();
+       ClienteDAO clienteDao = new ClienteDAO();
+       
+       cliente.setCodCliente(Integer.parseInt(request.getParameter("cpf")));
+       try{
+           clienteDao.selectById(cliente);
+       }catch(SQLException ex){
+           Logger.getLogger(BuscarCliente.class.getName()).log(Level.SEVERE, null, ex);
+       }
+       String json = "{\"existeCliente\":";
+       if(cliente.getCodCliente() != 0){
+           json+= true+",";
+           json+= "\"cpf\":\""+cliente.getCodCliente()+"\"";
+           json+= "\"nome\":\""+cliente.getNome()+"\"";
+           json+= "\"endereco\":\""+cliente.getEndereco()+"\"";
+           json+= "\"estado\":\""+cliente.getEstado().getUf()+"\"";
+           json+= "\"cidade\":\""+cliente.getCidade().getDescricao()+"\"";
+           json+= "\"telefone\":\""+cliente.getTelefone()+"\"";
+           json+= "\"celular\":\""+cliente.getCelular()+"\"";
+           json+= "\"periodo\":\""+cliente.getPeriodo()+"\"";
+           json+= "\"cpf\":\""+cliente.getCodCliente()+"\"";
+       }
     }
 }
