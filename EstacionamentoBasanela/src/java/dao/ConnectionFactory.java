ca35b8c70ebdab4341707d8579641c2bddce1767
alvaroprog <alvaro.prog@gmail.com>
2014-05-20 07:44:02 -0300

    Bug control

--- EstacionamentoBasanela/src/java/dao/ConnectionFactory.java
+++ EstacionamentoBasanela/src/java/dao/ConnectionFactory.java
@@ -17,17 +17,10 @@ import java.sql.Statement;
 public class ConnectionFactory {
 
     private static final String STR_DRIVER = "com.mysql.jdbc.Driver";
-<<<<<<< HEAD
     private static final String DATABASE = "basanelapark";//basanelapark
     private static final String STR_CON = "jdbc:mysql://127.0.0.1:3306/" + DATABASE;//127.0.0.1:3306/
     private static final String USER = "root";//root
     private static final String PASSWORD = "19930730";//19930730
-=======
-    private static final String DATABASE = "basanelapark";
-    private static final String STR_CON = "jdbc:mysql://127.0.0.1:3306/" + DATABASE;
-    private static final String USER = "root";
-    private static final String PASSWORD = "";//19930730
->>>>>>> c1d52b73abbaff3725283169d9c4428db710efc3
 
     public static Connection getConexao() {
         Connection conn = null;
